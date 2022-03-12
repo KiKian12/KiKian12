80235b424ed8aa1d476f98312b7aba61e7f567af
KiKian12 <101476609+KiKian12@users.noreply.github.com>
2022-03-12 20:30:40 +0100

    Add files via upload

---
+++ Soubory2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Soubory2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = File.ReadAllText("Cesta.txt");
+            bool b = true;
+            int i = 0;
+            while (b != false)
+            {
+                i++;
+                File.Create(path + i);
+            }
+        }
+    }
+}
